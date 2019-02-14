61335b0e4f8cff33bc7873a7b1ff06207fe376b5
unknown <AB049619@AB.COM>
2019-02-14 14:16:58 +0800

    add

--- AutoTest5/Chapter5/src/main/java/com/course/testng/BasicAnnotation.java
+++ AutoTest5/Chapter5/src/main/java/com/course/testng/BasicAnnotation.java
@@ -1,10 +1,27 @@
 package com.course.testng;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class BasicAnnotation {
     @Test
     public void testCase1(){
         System.out.println("This is testCase1");
+    }
+
+    @Test
+    public void testCase2(){
+        System.out.println("This is testCase2");
+    }
+
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("beforeMethod这是方法之前运行的");
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("afterMethod这是方法之前运行的");
     }
 }

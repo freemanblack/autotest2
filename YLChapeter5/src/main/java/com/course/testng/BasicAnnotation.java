b9a35b892c1da2e01552113cfbb3d4c9a0bb5657
yu2liang <yuliang9919@126.com>
2019-02-28 11:19:41 +0800

    TestNG-yl

---
+++ YLChapeter5/src/main/java/com/course/testng/BasicAnnotation.java
@@ -0,0 +1,10 @@
+package com.course.testng;
+
+import org.testng.annotations.Test;
+
+public class BasicAnnotation {
+    @Test
+    public void testCase1(){
+        System.out.println("于亮测试1");
+    }
+}
